3cb480a2c5dbcd2e0302447b9757d7b044ccb8d1
Stephan <stephan.semerad@gmail.com>
2022-04-15 00:33:18 +0200

    test fixed all

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-import os, csv
+import os
+import csv
 from datetime import datetime
 from sqlite3 import Timestamp
+from turtle import title
 from flask import Flask
 from flask import request, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from itsdangerous import json
 import pandas as pd
-from sqlalchemy import engine_from_config
-from sqlalchemy import String
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 from helpers.allowed_files import allowed_file
+from sqlalchemy import String, Numeric, Integer, Float, DateTime
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "static"
@@ -24,20 +25,36 @@ def index():
 
 
 def data_table_exists():
-    sql = f"SELECT * FROM sqlite_master WHERE name='data';"
-    result = db.session.execute(sql)
-    result = [x for x in result]
-    if result:
+    sql = f"PRAGMA table_info(data)"
+    headers = db.session.execute(sql)
+    headers = [x[1] for x in headers]
+    if headers:
         return True
     else:
         return False
+
+
+def adjust_data_types(df):
+    data_types = df.dtypes.apply(lambda x: x.name).to_dict()
+    for i in data_types:
+        if data_types[i] == "object":
+            data_types[i] = String
+        elif data_types[i] == "int64":
+            data_types[i] = Integer
+        elif data_types[i] == "float64":
+            data_types[i] = Numeric
+        elif data_types[i] == "datetime64[ns]":
+            data_types[i] = DateTime
+        else:
+            data_types[i] = String
+    return data_types
 
 
 def csv_to_table(file_path):
     print("csv_to_table")
     # 1 check if table exists if yes delete it.
     # 2. delete table if exists - so that we can upload new.
-    if data_table_exists:
+    if data_table_exists():
         try:
             result = db.session.execute(f"drop table data")
             db.session.commit()
@@ -45,7 +62,8 @@ def csv_to_table(file_path):
             print(e)
 
     df = pd.read_csv(file_path)
-    df.to_sql("data", con=db.engine, index=False)
+    data_types = adjust_data_types(df)
+    df.to_sql("data", con=db.engine, index=False, dtype=data_types)
 
     return {
         "status": "ok",
@@ -78,107 +96,102 @@ def file_upload():
 
 @app.route("/diplay_table_data", methods=["GET"])
 def diplay_table_data():
+    print('data_table_exists')
+    print(data_table_exists())
+    if not data_table_exists():
+        return jsonify(
+            data={
+                "headers": [],
+                "data": [],
+            }
+        )
+    else:
+        # 1 - get table description
 
-    # 1 - get table description
-    sql = f"PRAGMA table_info(data)"
-    headers = db.session.execute(sql)
-    headers = [x[1] for x in headers]
+        sql = f"PRAGMA table_info(data)"
+        headers = db.session.execute(sql)
+        headers = [x[1] for x in headers]
 
-    # 2 - get data from table
-    sql = f"select * from data"
-    data = db.session.execute(sql)
-    data = [list(x) for x in data]
+        print('headers:', headers)
 
-    data = {
-        "headers": headers,
-        "data": data,
-    }
-    return jsonify(data)
+        # 2 - get data from table
+        sql = f"select * from data"
+        data = db.session.execute(sql)
+        data = [list(x) for x in data]
+
+        data = {
+            "headers": headers,
+            "data": data,
+        }
+        return jsonify(data)
 
 
 @app.route("/display_table_meta", methods=["GET"])
 def display_table_meta():
-    # 1 - get table description
-    sql = f"PRAGMA table_info(data)"
-    result = db.session.execute(sql)
-    result = [[x[1], x[2]] for x in result]
+    if not data_table_exists:
+        return jsonify([])
+    else:
+        # 1 - get table description
+        sql = f"PRAGMA table_info(data)"
+        result = db.session.execute(sql)
+        result = [[x[1], x[2]] for x in result]
 
-    return jsonify(result)
+        return jsonify(result)
 
 
 @app.route("/change_column_type", methods=["PUT"])
 def change_column_type():
-    print("change_column_type")
 
     data = request.json
     column = data.get("column", None)
     datatype = data.get("datatype", None)
-    column = column[0][0] if column else None
+
+    print()
+    print("change_column_type")
+    print('column: ', column)
+    print('datatype: ', datatype)
 
     df = pd.read_sql_query("SELECT * from data", con=db.engine)
 
-    print("-----------------------------")
-    print("BEFORE")
-    print(df.dtypes)
-
-    # try:
-    if datatype == "BIGINT":
-        print("changing to BIGINT")
-        df[column] = df[column].astype("int64")
-
-    if datatype == "TEXT":
-        print("changing to TEXT")
-        df[column] = df[column].astype("object")
-
-    if datatype == "FLOAT":
-        print("changing to FLOAT")
-        df[column] = df[column].astype("float64")
-
-    print("AFTER")
-    print(df.dtypes)
-
-    # except Exception as e:
-    #     return jsonify(
-    #         {
-    #             "status": "notok",
-    #             "msg": f"column {column} can not be changed to data type {datatype}",
-    #         }
-    #     )
+    mapping = {
+        'VARCHAR': "object",
+        'INTEGER': "int64",
+        'FLOAT': "float64",
+    }
 
     try:
-        result = db.session.execute(f"drop table data")
-        db.session.commit()
+        df[column] = df[column].astype(mapping[datatype])
     except Exception as e:
-        print(e)
+        return jsonify({"status": "notok",  "msg": f"column {column} can not be changed to {datatype}, {e}"})
 
-    df.to_sql("data", con=db.engine, index=False, dtype={"IDBIAT": String})
-
+    result = db.session.execute(f"drop table data")
     db.session.commit()
 
-    print("AFTER AFTER ")
-    df = pd.read_sql_query("SELECT * from data", con=db.engine)
-    print(df.dtypes)
+    data_types = adjust_data_types(df)
+    df.to_sql("data", con=db.engine, index=False, dtype=data_types)
 
-    print("-----------------------------")
-
-    return jsonify(
-        {
-            "status": "ok",
-            "msg": f"column was changed",
-        }
-    )
+    return jsonify({"status": "ok",  "msg": f"column {column} was changed to {datatype}", })
 
 
-@app.route("/delete_column", methods=["GET"])
+@app.route("/delete_column", methods=["DELETE"])
 def delete_column():
     print("delete_column")
-    data = request.json
-    print(request.args)
-    print(request.form)
+    data = request.args
+    column = data.get("column", None)
+    print('column: ', column)
 
-    # column = data.get("column", None)
+    sql = f"PRAGMA table_info(data)"
+    data = db.session.execute(sql)
+    data = [x[1] for x in data]
+    print(data)
 
-    # print("column: ", column)
+    # check if its the last column.
+    if [column] == data:
+        result = db.session.execute(f"drop table data")
+        db.session.commit()
+    else:
+        result = db.session.execute(f"alter table data drop column {column}")
+        db.session.commit()
 
     return jsonify(
         {
